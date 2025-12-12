8fbe6bb1c9446d74d464d3c9086be1af762b721d
Rafael <rafcor.2005@gmail.com>
2025-12-12 13:56:41 +0100

    12

--- hapnow-angular/src/app/features/eventos/detalle-evento/detalle-evento.ts
+++ hapnow-angular/src/app/features/eventos/detalle-evento/detalle-evento.ts
@@ -1,35 +1,28 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { EventoService } from '../../../core/services/evento.service';
 import { Evento } from '../../../shared/models/evento.model';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-detalle-evento',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterLink, NavbarComponent],
   templateUrl: './detalle-evento.html',
   styleUrl: './detalle-evento.scss',
 })
-export class DetalleEventoComponent {
+export class DetalleEventoComponent implements OnInit {
   
   private route = inject(ActivatedRoute);
   private eventoService = inject(EventoService);
 
   evento: Evento | null = null;
-  loading = true;
+  cargando = true;
 
-  constructor() {
-    this.obtenerDetalleEvento();
-  }
 
-  async obtenerDetalleEvento(): Promise<void> {
-    this.route.paramMap.subscribe(async (params) => {
-      const id = params.get('id');
-      if (id) {
-        this.evento = await this.eventoService.obtenerEventoPorId(id);
-      }
-      this.loading = false;
-    })
+  async ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.evento = await this.eventoService.obtenerEventoPorId(id);
+    this.cargando = false;
   }
 }
--- hapnow-angular/src/app/shared/components/navbar/navbar.ts
+++ hapnow-angular/src/app/shared/components/navbar/navbar.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
